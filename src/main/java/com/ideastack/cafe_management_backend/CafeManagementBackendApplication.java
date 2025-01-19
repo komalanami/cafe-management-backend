4d91784140dcb8a7452e1b460e490d1269b2a66e
komalanami <kumarikomal1511@gmail.com>
2025-01-19 23:15:38 +0530

    initial commit

---
+++ src/main/java/com/ideastack/cafe_management_backend/CafeManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ideastack.cafe_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideastack/cafe_management_backend/CafeManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideastack.cafe_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
